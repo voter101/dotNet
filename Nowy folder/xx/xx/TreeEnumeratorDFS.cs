587e937e002e26a5233f652fc95b7cd2b409eaf9
Paweł Szymański <3x4to9@gmail.com>
2014-04-28 12:34:02 +0200

    d

---
+++ Nowy folder/xx/xx/TreeEnumeratorDFS.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zad2
+{
+    class TreeEnumeratorDFS<T> : IEnumerator<T> where T: IComparable<T>
+    {
+        private BinaryTreeNode<T> currentData = null;
+        private T currentItem = default(T);
+        private Queue<T> enumData = null;
+
+        public TreeEnumeratorDFS(BinaryTreeNode<T> data)
+        {
+            this.currentData = data;
+        }
+
+        public void kolejkowanie(Queue<T> enumQueue, BinaryTreeNode<T> tree)
+        {
+            if (tree.leftNode != null)
+            {
+                kolejkowanie(enumQueue, tree.leftNode);
+            }
+            enumQueue.Enqueue(tree.data);
+
+            if (tree.rightNode != null)
+            {
+                kolejkowanie(enumQueue, tree.rightNode);
+            }
+        }
+
+        #region IEnumerator<T> Members
+
+        T IEnumerator<T>.Current
+        {
+            get
+            {
+                if (this.enumData == null)
+                {
+                    throw new InvalidOperationException("Najpierw uzyj MoveNext()");
+                }
+                return this.currentItem;
+            }
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        void IDisposable.Dispose()
+        {
+           // throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IEnumerator Members
+
+        object System.Collections.IEnumerator.Current
+        {
+            get {
+                if (this.enumData == null)
+                {
+                    throw new InvalidOperationException("Najpierw uzyj MoveNext()");
+                }
+                return this.currentItem;
+                }
+        }
+
+        bool System.Collections.IEnumerator.MoveNext()
+        {
+            if (this.enumData == null)
+            {
+                this.enumData = new Queue<T>();
+                kolejkowanie(this.enumData, this.currentData);
+            }
+            if (this.enumData.Count > 0)
+            {
+                this.currentItem = this.enumData.Dequeue();
+                return true;
+            }
+            return false;
+        }
+
+        void System.Collections.IEnumerator.Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Nowy folder/zad1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int LiczbaProb = 50000000;
+            
+            
+            ArrayList arrayList = new ArrayList();
+            List<string> listT = new List<string>();
+            Hashtable hashTable = new Hashtable();
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            
+            
+            
+            Console.WriteLine("ArrayList vs List<T>: \n");
+            DateTime Start = DateTime.Now;
+            Console.WriteLine("Dodawanie elementow: ");
+            Console.WriteLine("ArrayList: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                arrayList.Add("1");
+            }
+            DateTime EndArrayAdd = DateTime.Now;
+            TimeSpan Czas = EndArrayAdd - Start;
+            Console.WriteLine(Czas);
+            Start = DateTime.Now;
+            Console.WriteLine("List<T>: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                listT.Add("1");
+            }
+            DateTime EndListT = DateTime.Now;
+            Czas = EndListT - Start;
+            Console.WriteLine(Czas);
+            Console.WriteLine("Przegladanie: ");
+            Console.WriteLine("ArrayList: ");
+            string a;
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                a = (string)arrayList[proba];
+            }
+            DateTime EndArrayAdd2 = DateTime.Now;
+            Czas = EndArrayAdd2 - EndListT;
+            Console.WriteLine(Czas);
+            Console.WriteLine("List<T>: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                a = listT[proba];
+            }
+            DateTime EndListT2 = DateTime.Now;
+            Czas = EndListT2 - EndArrayAdd2;
+            Console.WriteLine(Czas);
+            Console.WriteLine("Usuwanie: ");
+            Console.WriteLine("ArrayList: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                arrayList.RemoveAt(arrayList.Count - 1);
+            }
+            EndArrayAdd = DateTime.Now;
+            Czas = EndArrayAdd - EndListT2;
+            Console.WriteLine(Czas);
+            Console.WriteLine("List<T>: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                listT.RemoveAt(listT.Count-1);
+            }
+            EndListT = DateTime.Now;
+            Czas = EndListT - EndArrayAdd;
+            Console.WriteLine(Czas);
+
+
+
+
+
+            LiczbaProb = 6000000;
+
+            Console.WriteLine("Hashtable vs Dictionary: \n");
+            Start = DateTime.Now;
+            Console.WriteLine("Dodawanie elementow: ");
+            Console.WriteLine("Hashtable: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                hashTable.Add(proba, "a");
+            }
+            EndArrayAdd = DateTime.Now;
+            Czas = EndArrayAdd - Start;
+            Console.WriteLine(Czas);
+            Console.WriteLine("Dictionary: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                dictionary.Add(proba, "a");
+            }
+            EndListT = DateTime.Now;
+            Czas = EndListT - EndArrayAdd;
+            Console.WriteLine(Czas);
+            Console.WriteLine("Przegladanie: ");
+            Console.WriteLine("Hashtable: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                a = (string)hashTable[proba];
+            }
+            EndArrayAdd2 = DateTime.Now;
+            Czas = EndArrayAdd2 - EndListT;
+            Console.WriteLine(Czas);
+            Console.WriteLine("Dictionary: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                a = dictionary[proba];
+            }
+            EndListT2 = DateTime.Now;
+            Czas = EndListT2 - EndArrayAdd2;
+            Console.WriteLine(Czas);
+            Console.WriteLine("Usuwanie: ");
+            Console.WriteLine("Hashtable: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                hashTable.Remove(proba);
+            }
+            EndArrayAdd = DateTime.Now;
+            Czas = EndArrayAdd - EndListT2;
+            Console.WriteLine(Czas);
+            Console.WriteLine("Dictionary: ");
+            for (int proba = 0; proba < LiczbaProb; proba++)
+            {
+                dictionary.Remove(proba);
+            }
+            EndListT = DateTime.Now;
+            Czas = EndListT - EndArrayAdd;
+            Console.WriteLine(Czas);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
