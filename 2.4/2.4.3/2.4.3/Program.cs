a01950aa2679bd3923b30366b5c69ac6a3d2867b
Wiktor Mociun <voter101@gmail.com>
2014-05-06 11:25:08 +0200

    I guess that's simpler. ;)

--- 2.4/2.4.3/2.4.3/Program.cs
+++ 2.4/2.4.3/2.4.3/Program.cs
@@ -10,14 +10,13 @@ namespace _2._4._3
     {
         static void Main(string[] args)
         {
-            var list = System.IO.File.ReadAllLines(@"data.txt");
-            var grupy = from nazwisko in list
+            var grupy = from nazwisko in System.IO.File.ReadAllLines(@"data.txt")
                         orderby nazwisko
                         group nazwisko by nazwisko[0] into g
-                        select new { pierwsza = g.Key};
+                        select g.Key;
             foreach (var g in grupy)
             {
-                Console.WriteLine(g.pierwsza);
+                Console.WriteLine(g);
             }
             Console.ReadKey();
         }
