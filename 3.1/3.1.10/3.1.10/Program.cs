64bc5c89d24452927258aaf755b3e50343be60af
TrzyRazyCztery <3x4to9@gmail.com>
2014-05-11 21:52:57 +0200

    3.1.10
    czytanie obiektów korzystajac z refleksji

---
+++ 3.1/3.1.10/3.1.10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            Console.WriteLine(reader.read("temp"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3.1/3.1.10/3.1.10/Reader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace _3._1._10
+{
+    class Reader
+    {
+        public string read(string name)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = String.Format("_3._1._10.Resources.{0}.txt", name);
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+    }
+}
