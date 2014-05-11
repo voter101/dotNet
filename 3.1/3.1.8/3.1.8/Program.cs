dac20929dd04ca96616f8ab77ad34157cc99d7bc
TrzyRazyCztery <3x4to9@gmail.com>
2014-05-11 20:48:46 +0200

    3.1.9
    globalizacja

--- 3.1/3.1.8/3.1.8/Program.cs
+++ 3.1/3.1.8/3.1.8/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace _3._1._8
 {
@@ -10,6 +10,48 @@ namespace _3._1._8
     {
         static void Main(string[] args)
         {
+            DateTime data = new DateTime();
+            data = DateTime.Now;
+
+            List<CultureInfo> listaJezykow = new List<CultureInfo>();
+            listaJezykow.Add(new CultureInfo("en"));
+            listaJezykow.Add(new CultureInfo("de"));
+            listaJezykow.Add(new CultureInfo("fr"));
+            listaJezykow.Add(new CultureInfo("ru"));
+            listaJezykow.Add(new CultureInfo("ar"));
+            listaJezykow.Add(new CultureInfo("cs"));
+            listaJezykow.Add(new CultureInfo("pl"));
+
+            foreach (CultureInfo jezyk in listaJezykow)
+            {
+                Console.WriteLine("{0} \n Pełna nazwa Miesięcy: ", jezyk.EnglishName);
+                foreach (string miesiac in jezyk.DateTimeFormat.MonthNames)
+                {
+                    Console.WriteLine(miesiac);
+                }
+                Console.WriteLine("\nSkrótowa nazwa Miesięcy: ");
+                foreach (string miesiac in jezyk.DateTimeFormat.AbbreviatedMonthNames)
+                {
+                    Console.WriteLine(miesiac);
+                }
+                Console.WriteLine("\nPełna nazwa Dni: ");
+                foreach (string dzien in jezyk.DateTimeFormat.DayNames)
+                {
+                    Console.WriteLine(dzien);
+                }
+                Console.WriteLine("\nSkrótowe nazwy dni:");
+                foreach (string miesiac in jezyk.DateTimeFormat.AbbreviatedDayNames)
+                {
+                    Console.WriteLine(miesiac);
+                }
+                Console.WriteLine("\n{0}", data.ToString("D", jezyk));
+                Console.WriteLine();
+
+            }
+
+           
+
+            Console.ReadKey();
         }
     }
 }
